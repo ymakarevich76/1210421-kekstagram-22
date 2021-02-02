360ac1e48b779f5959bb2b45596b4f69a324baa8
Юля <ymakarevich76@gmail.com>
2021-02-02 16:52:07 +0300

    Add 2 function

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntegerNumber = (min, max) => {
+  if(min >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  else if(min < 0) {
+    alert('Введено отрицательное число');
+  }
+  else {
+    alert('Неправильно задан диапазон чисел');
+  }
+}
+getRandomIntegerNumber(1,10);
+
+const getLengthComment = (checkString, maxLength) => {
+  if(checkString.length <= maxLength) {
+    return true;
+  } return false;
+}
+getLengthComment('Юля', 2);
