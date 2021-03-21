cd7af8fdbdf730c1e38992e7b253f1e62f779bd3
Юля <ymakarevich76@gmail.com>
2021-03-21 20:27:33 +0300

    Delete last comment

--- source/js/random.js
+++ source/js/random.js
@@ -2,7 +2,7 @@ const getRandomIntegerNumber = (min, max) => {
   if (min >= 0 && min < max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   } else if (min < 0) {
     alert('Введено отрицательное число');
   } else {
